5f09a232fb8c44e587f6d97eccfe4d86b071f482
Crowdbotics <team@crowdbotics.com>
2023-08-16 17:28:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ezjobfinder-42884",
+  url: "https://ezjobfinder-42884.botics.co",
+  api: "https://ezjobfinder-42884.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
